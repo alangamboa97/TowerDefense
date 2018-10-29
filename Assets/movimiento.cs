a884352163dc5535640ef51e69d113c3e7e7e3b5
Alan Gamboa <alangam97@gmail.com>
2018-10-28 19:19:52 -0600

    Ya se mueve la bolita

---
+++ Assets/movimiento.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class movimiento : MonoBehaviour {
+
+    public float velocidad = 10f;
+
+    private Transform objetivo;
+    private int indice_wavepoint = 0;
+
+    void Start()
+    {
+        objetivo = Waypoints.waypoints[0];
+
+    }
+
+    void Update()
+    {
+        Vector3 dir = objetivo.position - transform.position;
+        transform.Translate(dir.normalized * velocidad * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, objetivo.position) <= 0.4f)
+        {
+            GetNextWaypoint();
+
+
+        }
+    }
+
+        void GetNextWaypoint()
+        {
+            if(indice_wavepoint >= Waypoints.waypoints.Length - 1)
+            {
+                Destroy(gameObject);
+            }
+            indice_wavepoint++;
+            objetivo = Waypoints.waypoints[indice_wavepoint];
+
+        }
+
+
+       
+    }
+	
+
