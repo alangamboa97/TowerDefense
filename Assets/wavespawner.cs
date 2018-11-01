b74c166f663f07be481d1f7176b20ce156de0d07
Alan Gamboa <alangam97@gmail.com>
2018-11-01 11:07:31 -0600

    Comenzando con el funcionamiento de las torretas

--- Assets/wavespawner.cs
+++ Assets/wavespawner.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class wavespawner : MonoBehaviour {
@@ -8,26 +9,33 @@ public class wavespawner : MonoBehaviour {
     public Transform enemyPrefab;
     public Transform puntodespawn;
     public float tiempo_entre_olas = 6f;
+
+    public Text contadorTexto;
     private float contador = 2f;
-    private int numoleadas = 1;
+    private int numoleadas = 0;
 
     void Update()
     {
         if(contador<= 0f)
         {
-            Spawn();
+            StartCoroutine(Spawn());
             contador = tiempo_entre_olas;
         }
         contador -= Time.deltaTime;
+
+        contadorTexto.text = Mathf.Floor(contador).ToString();
     }
 
-    void Spawn()
+    IEnumerator Spawn()
+         
     {
+        numoleadas++;
         for (int i = 0; i < numoleadas; i++)
         {
             SpawnEnemigo();
+            yield return new WaitForSeconds(0.8f);
         }
-        numoleadas++;
+       
 
         Debug.Log("Una oleada se aproxima");
     }
