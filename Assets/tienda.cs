4ef9d7e861ac1df970489b4ed3a255bbb9c634a3
Alan Gamboa <alangam97@gmail.com>
2018-11-05 09:28:25 -0600

    Empezando con el scripting de la tienda

---
+++ Assets/tienda.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tienda : MonoBehaviour {
+
+    public void comprarTorretaStandard()
+    {
+        Debug.Log("Has compraro torreta");
+    }
+
+    public void comprarTorretaN2()
+    {
+
+    }
+}
